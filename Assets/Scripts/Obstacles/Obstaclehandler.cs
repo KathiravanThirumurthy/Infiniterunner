b94303bd913ddea9a309f36dd85daef2d966f1cd
Kathiravan Thirumurthy <tkathiravan1976@gmail.com>
2022-04-06 13:24:14 +0530

    coin pickup audio is implemented

--- Assets/Scripts/Obstacles/Obstaclehandler.cs
+++ Assets/Scripts/Obstacles/Obstaclehandler.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 
 public class Obstaclehandler : MonoBehaviour
 {
-    [SerializeField]
+  /*  [SerializeField]
     private GameObject _barryGuardPrefab;
     [SerializeField]
      private GameObject _firePrefab;
     [SerializeField]
-     private GameObject _obstaclePrefab;
+     private GameObject _obstaclePrefab;*/
     [SerializeField]
     private GameObject _coinPrefab;
     [SerializeField]
@@ -17,7 +17,7 @@ public class Obstaclehandler : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        for (int i = 0; i < Random.Range(5, 10); i++)
+        for (int i = 0; i < Random.Range(5, 15); i++)
         {
             createCoin();
         }
@@ -43,29 +43,26 @@ public class Obstaclehandler : MonoBehaviour
     }
     private void createCoin()
     {
-        // GameObject temp = Instantiate(_obstaclePrefab, new Vector3(Random.Range(-2.0f, 2.0f), 1, Random.Range(transform.position.z - 20, transform.position.z + 20)), Quaternion.identity);
-        //temp.transform.parent = gameObject.transform;
         GameObject temp = Instantiate(_coinPrefab, new Vector3(Random.Range(-2.8f, 2.8f), 1, Random.Range(transform.position.z - 20, transform.position.z + 50)), Quaternion.identity);
         temp.transform.parent = _platformPrefab.transform;
     }
-   private void createObstacle()
+  /* private void createObstacle()
     {
-        // GameObject temp = Instantiate(_obstaclePrefab, new Vector3(Random.Range(-2.0f, 2.0f), 1, Random.Range(transform.position.z - 20, transform.position.z + 20)), Quaternion.identity);
-        //temp.transform.parent = gameObject.transform;
+       
         GameObject temp = Instantiate(_obstaclePrefab, new Vector3(Random.Range(-2.0f, 2.0f), 0.5f, Random.Range(transform.position.z - 20, transform.position.z + 20)), Quaternion.identity);
         temp.transform.parent = _platformPrefab.transform;
-    }
-    private void createFire()
+    }*/
+    /*private void createFire()
     {
         Debug.Log("fire");
         // GameObject temp = Instantiate(_obstaclePrefab, new Vector3(Random.Range(-2.0f, 2.0f), 1, Random.Range(transform.position.z - 20, transform.position.z + 20)), Quaternion.identity);
         //temp.transform.parent = gameObject.transform;
         GameObject temp = Instantiate(_firePrefab, new Vector3(Random.Range(-2.0f, 2.0f),1, Random.Range(transform.position.z -20, transform.position.z + 20)), Quaternion.identity);
         temp.transform.parent = _platformPrefab.transform;
-    }
-    private void createBarryGuard()
+    }*/
+   /* private void createBarryGuard()
     {
         GameObject temp = Instantiate(_barryGuardPrefab, new Vector3(Random.Range(-2.9f, -0.8f), 0.2f, Random.Range(transform.position.z - 20, transform.position.z + 20)), Quaternion.identity);
         temp.transform.parent = _platformPrefab.transform;
-    }
+    }*/
 }
